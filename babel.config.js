5fa1d4fb032f662029f4357ad77204c0c5d15dec
Adtx <adrianodiasteixeira@gmail.com>
2022-11-06 22:55:00 +0000

    build: Add packages and configs

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+/* module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+}; */
+
+module.exports = {
+  presets: [
+    "@babel/preset-react",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+  ],
+}
