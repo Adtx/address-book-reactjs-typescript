ba6d72c83c4204bdb2419bbd0d953d0a80806dde
Adtx <adrianodiasteixeira@gmail.com>
2022-11-11 16:50:30 +0000

    feature: Add routing for home and settings page

--- src/AddressBook.tsx
+++ src/AddressBook.tsx
@@ -1,6 +1,15 @@
 import React from "react"
 import Home from "./pages/Home/Home"
+import Settings from "./pages/Settings/Settings"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 export default function AddressBook() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  )
 }
